68264d4c0f3a8e9879484543794f180f0711601c
Chris Doggett <doggettc@gmail.com>
2025-01-02 01:02:14 -0600

    Added catppuccin theme and highlight when yanking

---
+++ .config/nvim/lua/plugins/themes/catppuccin.lua
@@ -0,0 +1,26 @@
+return {
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			flavour = "frappe",
+			transparent_background = true,
+			term_colors = true,
+			integrations = {
+				cmp = true,
+				gitsigns = true,
+				nvimtree = true,
+				treesitter = true,
+				notify = true,
+				mini = {
+					enabled = true,
+					indentscope_color = "",
+				},
+			},
+		})
+
+		-- setup must be called before loading
+		vim.cmd.colorscheme("catppuccin")
+	end,
+}
