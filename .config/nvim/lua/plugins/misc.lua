85991307ef81219271d9f6c253c5468e859fde15
Chris Doggett <doggettc@gmail.com>
2025-01-13 23:20:21 -0600

    nvim-surround

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -79,9 +79,13 @@ return {
 	},
 	{
 		"kylechui/nvim-surround",
-		event = { "BufReadPre", "BufNewFile" },
-		version = "*",
-		config = true,
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
 	},
 	{
 		"vim-test/vim-test",
