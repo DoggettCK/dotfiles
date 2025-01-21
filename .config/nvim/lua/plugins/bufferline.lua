7cdfd028e9942737217ef3b2df69a04a3cb46789
Chris Doggett <doggettc@gmail.com>
2025-01-21 12:35:57 -0600

    Playing with selected colors

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -9,9 +9,9 @@ return {
 
 		require("bufferline").setup({
 			options = {
-				mode = "buffers", -- set to "tabs" to only show tabpages instead
-				themable = true, -- allows highlight groups to be overriden i.e. sets highlights as default
-				numbers = "none", -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
+				mode = "buffers",        -- set to "tabs" to only show tabpages instead
+				themable = true,         -- allows highlight groups to be overriden i.e. sets highlights as default
+				numbers = "none",        -- | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
 				close_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
 				right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
 				left_mouse_command = "buffer %d", -- can be a string | function, see "Mouse actions"
@@ -56,19 +56,19 @@ return {
 				buffer_selected = {
 					bold = true,
 					italic = false,
-					bg = "#BBBBBB",
+					bg = "#A9AEBF",
 					fg = "#434C5E",
 				},
 				close_button_selected = {
 					bold = true,
 					italic = false,
-					bg = "#BBBBBB",
+					bg = "#A9AEBF",
 					fg = "#434C5E",
 				},
 				modified_selected = {
 					bold = true,
 					italic = false,
-					bg = "#BBBBBB",
+					bg = "#A9AEBF",
 					fg = "#43AC5E",
 				},
 				-- separator_selected = {},
