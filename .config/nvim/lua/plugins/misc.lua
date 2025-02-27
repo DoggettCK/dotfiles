b0373d888bf6428f6bc4c874a82ed8e87af20127
Chris Doggett <doggettc@gmail.com>
2025-02-27 12:22:53 -0600

    add love2d

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -107,4 +107,14 @@ return {
 			require("devcontainer").setup({})
 		end,
 	},
+	{
+		"S1M0N38/love2d.nvim",
+		cmd = "LoveRun",
+		opts = {},
+		keys = {
+			{ "<leader>v", ft = "lua", desc = "LÖVE" },
+			{ "<leader>vv", "<cmd>LoveRun<cr>", ft = "lua", desc = "Run LÖVE" },
+			{ "<leader>vs", "<cmd>LoveStop<cr>", ft = "lua", desc = "Stop LÖVE" },
+		},
+	},
 }
