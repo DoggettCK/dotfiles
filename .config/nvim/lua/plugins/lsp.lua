993656e3315c4ae5f61ed65ce82147cabfe7317b
Chris Doggett <doggettc@gmail.com>
2025-03-07 23:38:25 -0600

    Took Elixir out of list of emmet_language_servers

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -168,7 +168,6 @@ return { -- LSP Configuration & Plugins
 					"scss",
 					"pug",
 					"heex",
-					"elixir",
 				},
 				-- Read more about this options in the [vscode docs](https://code.visualstudio.com/docs/editor/emmet#_emmet-configuration).
 				-- **Note:** only the options listed in the table are supported.
