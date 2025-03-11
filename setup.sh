92b0f312a6d60676bdb8a0cd4933d90468ec03a2
Chris Doggett <doggettc@gmail.com>
2025-03-11 13:35:37 -0500

    Remove Brave browser, fix path to bash (diff on NixOS)

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 if [[ $(command -v brew) == "" ]]; then
     echo "Installing Homebrew"
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    $(command -v bash) -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
     echo "Updating Homebrew"
     brew update
