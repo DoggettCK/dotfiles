5830d5ac5c47aafa55aebb0cfd7ef1ab00f39566
Chris Doggett <doggettc@gmail.com>
2025-03-11 17:21:01 -0500

    Cleaned up setup into functions

--- setup.sh
+++ setup.sh
@@ -10,6 +10,32 @@ install_or_update_brew() {
     fi
 }
 
+stow_package() {
+    echo "Symlinking ${1} config with stow"
+    stow -R -v -t ~ "$1"
+}
+
+stow_everything() {
+    stow_package "alacritty"
+    stow_package "asdf"
+    stow_package "fzf"
+    stow_package "git"
+    stow_package "kitty"
+    stow_package "neovim"
+    stow_package "tmux"
+    stow_package "vim"
+    stow_package "zsh"
+}
+
+install_languages_with_asdf() {
+    # Install programming languages via ASDF (versions in .tool-versions)
+    echo "Installing plugins and languages via ASDF specified in ~/.tool-versions"
+    cut -d ' ' -f 1 <asdf/.tool-versions |
+        tr '\n' '\0' |
+        xargs -0 -n1 asdf plugin add
+    asdf install
+}
+
 # Do OS-specific software installs
 shopt -s nocasematch
 case $(uname -a) in
@@ -22,51 +48,36 @@ esac
 case "$OS" in
 NixOS)
     echo "NixOS-specific installation"
+    echo "Symlinking configs via Stow"
+    stow_everything
     echo "Brew currently unsupported on NixOS, install via /etc/nixos/configuration.nix"
     # TODO: add instructions to output NixOS packages list
+    # TODO: Make font warning
     echo "Download JetBrainsMono Nerd Font from https://www.nerdfonts.com and install via system"
     ;;
 Darwin)
     echo "OSX-specific installation"
+    echo "Installing or updating Brew"
     install_or_update_brew
     echo "Installing packages via Brew"
     brew bundle
+    echo "Symlinking configs via Stow"
+    stow_everything
+    echo "Installing languages via ASDF"
+    install_languages_with_asdf
     ;;
 WSL)
     echo "WSL-specific installation"
+    echo "Installing or updating Brew"
     install_or_update_brew
     echo "Installing packages via Brew"
     brew bundle
+    echo "Symlinking configs via Stow"
+    stow_everything
+    echo "Installing languages via ASDF"
+    install_languages_with_asdf
     ;;
 esac
-
-# Symlink everything
-echo "Symlinking Alacritty config with stow"
-stow -R -v -t ~ alacritty
-echo "Symlinking ASDF config with stow"
-stow -R -v -t ~ asdf
-echo "Symlinking FZF config with stow"
-stow -R -v -t ~ fzf
-echo "Symlinking Git config with stow"
-stow -R -v -t ~ git
-echo "Symlinking Kitty config with stow"
-stow -R -v -t ~ kitty
-echo "Symlinking Neovim config with stow"
-stow -R -v -t ~ neovim
-echo "Symlinking Tmux config with stow"
-stow -R -v -t ~ tmux
-echo "Symlinking Vim config with stow"
-stow -R -v -t ~ vim
-echo "Symlinking ZSH config with stow"
-stow -R -v -t ~ zsh
-
-# Install programming languages via ASDF (versions in .tool-versions)
-echo "Installing plugins and languages via ASDF specified in ~/.tool-versions"
-cat asdf/.tool-versions |
-    cut -d ' ' -f 1 |
-    tr '\n' '\0' |
-    xargs -0 -n1 asdf plugin add
-asdf install
 
 # Git submodules include fzf-git.sh and git-number
 echo "Updating git submodules"
