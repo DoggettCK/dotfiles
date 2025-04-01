7489492c7554c818a837ebfdb6d1077ff7e07e6a
Chris Doggett <doggettc@gmail.com>
2025-04-01 02:08:12 -0500

    Switch from telescope to fzf-lua

---
+++ neovim/.config/nvim/lua/plugins/fzf-lua.lua
@@ -0,0 +1,63 @@
+return {
+	"ibhagwan/fzf-lua",
+	dependencies = { "echasnovski/mini.icons" },
+	opts = {},
+	keys = {
+		{
+			"<leader>ff",
+			function()
+				require("fzf-lua").files()
+			end,
+			desc = "[F]ind [F]iles in project directory",
+		},
+		{
+			"<leader>fg",
+			function()
+				require("fzf-lua").live_grep()
+			end,
+			desc = "[F]ind with [G]rep in project directory",
+		},
+		{
+			"<leader>fc",
+			function()
+				require("fzf-lua").files({ cwd = vim.fn.stdpath("config") })
+			end,
+			desc = "[F]ind in NeoVim [C]onfiguration",
+		},
+		{
+			"<leader>fb",
+			function()
+				require("fzf-lua").builtin()
+			end,
+			desc = "[F]ind [B]uiltin FZF",
+		},
+		{
+			"<leader>fw",
+			function()
+				require("fzf-lua").grep_cword()
+			end,
+			desc = "[F]ind current [W]ord",
+		},
+		{
+			"<leader>fW",
+			function()
+				require("fzf-lua").grep_cWORD()
+			end,
+			desc = "[F]ind current [W]ORD",
+		},
+		{
+			"<leader>fr",
+			function()
+				require("fzf-lua").resume()
+			end,
+			desc = "[F]ind [R]esume",
+		},
+		{
+			"<leader><leader>",
+			function()
+				require("fzf-lua").buffers()
+			end,
+			desc = "Find Buffer",
+		},
+	},
+}
