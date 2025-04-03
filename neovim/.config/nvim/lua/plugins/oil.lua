4a6efb01372d6028626a9299956257e56b99c671
Chris Doggett <doggettc@gmail.com>
2025-04-02 19:58:06 -0500

    Bind backtick to toggle Oil floating window

--- neovim/.config/nvim/lua/plugins/oil.lua
+++ neovim/.config/nvim/lua/plugins/oil.lua
@@ -1,3 +1,5 @@
+vim.keymap.set("n", "`", "<cmd>Oil --float<CR>", { noremap = true, silent = true })
+
 return {
 	"stevearc/oil.nvim",
 	---@module 'oil'
@@ -7,18 +9,14 @@ return {
 		keymaps = {
 			["<C-s>"] = false, -- already bound to file saving
 			["<C-h>"] = false, -- want to retain window movement
+			["`"] = "<cmd>:wq<CR>", -- inside an Oil window, backtick to close
 		},
 		watch_for_changes = true,
 		view_options = {
 			show_hidden = true,
 		},
 	},
-	dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+	dependencies = { "nvim-tree/nvim-web-devicons" },
 	-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
 	lazy = false,
-	config = function()
-		require("oil").setup({
-			vim.keymap.set("n", "`", "<cmd>Oil --float<CR>", { noremap = true, silent = true }),
-		})
-	end,
 }
