e91a0a3530b06b98dbd1e0f6ff10faaafdf057e1
Chris Doggett <doggettc@gmail.com>
2025-04-03 23:16:12 -0500

    Bare-bones LSP

--- neovim/.config/nvim/lua/config/lazy.lua
+++ neovim/.config/nvim/lua/config/lazy.lua
@@ -18,9 +18,6 @@ vim.opt.rtp:prepend(lazypath)
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
-
 require("core.options") -- Load general options
 require("core.keymaps") -- Load general keymaps
 
