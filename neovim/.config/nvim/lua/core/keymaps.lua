23712868ce77942305116e70f5543747deb367a3
Chris Doggett <doggettc@gmail.com>
2025-04-04 01:21:19 -0500

    LSP/Formatting done, still needs autocomplete

--- neovim/.config/nvim/lua/core/keymaps.lua
+++ neovim/.config/nvim/lua/core/keymaps.lua
@@ -104,7 +104,7 @@ end)
 -- Diagnostic keymaps
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
-vim.keymap.set("n", "<leader>dm", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+vim.keymap.set("n", "<leader>df", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
 vim.keymap.set("n", "<leader>dl", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
 
 -- Easy escape to normal mode. No english words contain 'jf', and they're
