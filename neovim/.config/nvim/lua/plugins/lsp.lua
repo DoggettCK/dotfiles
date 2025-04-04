f0378466a3b0e25371ed920dedb4677615672399
Chris Doggett <doggettc@gmail.com>
2025-04-04 02:49:50 -0500

    LSP Completion finished (for now)

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -10,7 +10,21 @@ return {
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
 
 		-- Useful status updates for LSP.
-		{ "j-hui/fidget.nvim", opts = {} },
+		{
+			"j-hui/fidget.nvim",
+			opts = {
+				progress = {
+					display = {
+						done_icon = "✓", -- Icon shown when all LSP progress tasks are complete
+					},
+				},
+				notification = {
+					window = {
+						winblend = 0, -- Background color opacity in the notification window
+					},
+				},
+			},
+		},
 	},
 	config = function()
 		-- Brief aside: **What is LSP?**
@@ -189,8 +203,16 @@ return {
 		--  By default, Neovim doesn't support everything that is in the LSP specification.
 		--  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
 		--  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
-		local capabilities = vim.lsp.protocol.make_client_capabilities()
-		-- capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+		local capabilities = {
+			textDocument = {
+				foldingRange = {
+					dynamicRegistration = false,
+					lineFoldingOnly = true,
+				},
+			},
+		}
+
+		capabilities = require("blink.cmp").get_lsp_capabilities(capabilities)
 
 		-- Enable the following language servers
 		--  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
