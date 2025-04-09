fd532920f71845620cb9263baa58886c6daf34a4
Chris Doggett <doggettc@gmail.com>
2025-04-09 17:25:58 -0500

    Completion fixes

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -226,6 +226,30 @@ return {
 		local servers = {
 			bashls = {},
 			marksman = {},
+			emmet_language_server = {
+				filetypes = {
+					"css",
+					"eruby",
+					"html",
+					"javascript",
+					"javascriptreact",
+					"less",
+					"sass",
+					"scss",
+					"svelte",
+					"pug",
+					"typescriptreact",
+					"vue",
+				},
+				init_options = {
+					html = {
+						options = {
+							-- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+							["bem.enabled"] = true,
+						},
+					},
+				},
+			},
 			-- clangd = {},
 			-- gopls = {},
 			-- pyright = {},
