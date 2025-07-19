1972e90321bc2b52b700e59dd1600b738e3fbff9
Chris Doggett <doggettc@gmail.com>
2025-07-19 14:36:24 -0500

    Restored old blink.cmp

--- neovim/.config/nvim/lua/plugins/blink-cmp.lua
+++ neovim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -1,180 +1,107 @@
 return {
-	{ "L3MON4D3/LuaSnip", keys = {} },
-	{
-		"saghen/blink.cmp",
-		dependencies = {
-			"rafamadriz/friendly-snippets",
-		},
-		version = "*",
-		config = function()
-			require("blink.cmp").setup({
-				snippets = { preset = "luasnip" },
-				signature = { enabled = true },
-				appearance = {
-					use_nvim_cmp_as_default = false,
-					nerd_font_variant = "normal",
-				},
-				sources = {
-					default = { "lsp", "path", "snippets", "buffer" },
-					providers = {
-						cmdline = {
-							min_keyword_length = 2,
-						},
-					},
-				},
-				keymap = {
-					["<C-f>"] = {},
-				},
-				cmdline = {
-					enabled = false,
-					completion = { menu = { auto_show = true } },
-					keymap = {
-						["<CR>"] = { "accept_and_enter", "fallback" },
-					},
-				},
-				completion = {
-					menu = {
-						border = nil,
-						scrolloff = 1,
-						scrollbar = false,
-						draw = {
-							columns = {
-								{ "kind_icon" },
-								{ "label",      "label_description", gap = 1 },
-								{ "kind" },
-								{ "source_name" },
-							},
-						},
-					},
-					documentation = {
-						window = {
-							border = nil,
-							scrollbar = false,
-							winhighlight = 'Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,EndOfBuffer:BlinkCmpDoc',
-						},
-						auto_show = true,
-						auto_show_delay_ms = 500,
-					},
-				},
-			})
-
-			require("luasnip.loaders.from_vscode").lazy_load()
-		end,
+	"saghen/blink.cmp",
+	dependencies = {
+		"rafamadriz/friendly-snippets",
+		"onsails/lspkind.nvim",
 	},
+
+	version = "1.*",
+	opts = {
+		-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+		-- 'super-tab' for mappings similar to vscode (tab to accept)
+		-- 'enter' for enter to accept
+		-- 'none' for no mappings
+		--
+		-- All presets have the following mappings:
+		-- C-space: Open menu or open docs if already open
+		-- C-n/C-p or Up/Down: Select next/previous item
+		-- C-e: Hide menu
+		-- C-k: Toggle signature help (if signature.enabled = true)
+		--
+		-- See :h blink-cmp-config-keymap for defining your own keymap
+		keymap = {
+			["<Tab>"] = { "select_and_accept", "fallback" },
+			["<S-Enter>"] = { "select_accept_and_enter", "fallback" },
+		},
+
+		appearance = {
+			-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+			-- Adjusts spacing to ensure icons are aligned
+			nerd_font_variant = "mono",
+		},
+
+		-- (Default) Only show the documentation popup when manually triggered
+		completion = {
+			documentation = { auto_show = true, window = { border = "rounded" } },
+			menu = {
+				auto_show = true,
+				draw = {
+
+					columns = {
+						{ "label", gap = 1 },
+						{ "kind_icon", "kind", gap = 1 },
+					},
+					components = {
+						kind_icon = {
+							text = function(ctx)
+								local lspkind = require("lspkind")
+								local icon = ctx.kind_icon
+								if vim.tbl_contains({ "Path" }, ctx.source_name) then
+									local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
+									if dev_icon then
+										icon = dev_icon
+									end
+								else
+									icon = lspkind.symbolic(ctx.kind, {
+										mode = "symbol",
+									})
+								end
+
+								return icon .. ctx.icon_gap
+							end,
+
+							-- Optionally, use the highlight groups from nvim-web-devicons
+							-- You can also add the same function for `kind.highlight` if you want to
+							-- keep the highlight groups in sync with the icons.
+							highlight = function(ctx)
+								local hl = ctx.kind_hl
+								if vim.tbl_contains({ "Path" }, ctx.source_name) then
+									local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
+									if dev_icon then
+										hl = dev_hl
+									end
+								end
+								return hl
+							end,
+						},
+					},
+				},
+
+				border = "rounded",
+			},
+			ghost_text = { enabled = false },
+		},
+
+		-- Default list of enabled providers defined so that you can extend it
+		-- elsewhere in your config, without redefining it, due to `opts_extend`
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer" },
+			providers = {
+				buffer = {
+					opts = {
+						-- Complete from all open buffers
+						get_bufnrs = vim.api.nvim_list_bufs,
+					},
+				},
+			},
+		},
+
+		-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+		-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+		-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+		--
+		-- See the fuzzy documentation for more information
+		fuzzy = { implementation = "prefer_rust_with_warning" },
+	},
+	opts_extend = { "sources.default" },
 }
--- return {
--- 	"saghen/blink.cmp",
--- 	-- optional: provides snippets for the snippet source
--- 	dependencies = {
--- 		"rafamadriz/friendly-snippets",
--- 		"onsails/lspkind.nvim",
--- 	},
---
--- 	-- use a release tag to download pre-built binaries
--- 	version = "1.*",
--- 	-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
--- 	-- build = 'cargo build --release',
--- 	-- If you use nix, you can build from source using latest nightly rust with:
--- 	-- build = 'nix run .#build-plugin',
---
--- 	---@module 'blink.cmp'
--- 	---@type blink.cmp.Config
--- 	opts = {
--- 		-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
--- 		-- 'super-tab' for mappings similar to vscode (tab to accept)
--- 		-- 'enter' for enter to accept
--- 		-- 'none' for no mappings
--- 		--
--- 		-- All presets have the following mappings:
--- 		-- C-space: Open menu or open docs if already open
--- 		-- C-n/C-p or Up/Down: Select next/previous item
--- 		-- C-e: Hide menu
--- 		-- C-k: Toggle signature help (if signature.enabled = true)
--- 		--
--- 		-- See :h blink-cmp-config-keymap for defining your own keymap
--- 		keymap = {
--- 			["<Tab>"] = { "select_and_accept", "fallback" },
--- 			["<S-Enter>"] = { "select_accept_and_enter", "fallback" },
--- 		},
---
--- 		appearance = {
--- 			-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
--- 			-- Adjusts spacing to ensure icons are aligned
--- 			nerd_font_variant = "mono",
--- 		},
---
--- 		-- (Default) Only show the documentation popup when manually triggered
--- 		completion = {
--- 			documentation = { auto_show = true, window = { border = "rounded" } },
--- 			menu = {
--- 				auto_show = true,
--- 				draw = {
---
--- 					columns = {
--- 						{ "label", gap = 1 },
--- 						{ "kind_icon", "kind", gap = 1 },
--- 					},
--- 					components = {
--- 						kind_icon = {
--- 							text = function(ctx)
--- 								local lspkind = require("lspkind")
--- 								local icon = ctx.kind_icon
--- 								if vim.tbl_contains({ "Path" }, ctx.source_name) then
--- 									local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
--- 									if dev_icon then
--- 										icon = dev_icon
--- 									end
--- 								else
--- 									icon = lspkind.symbolic(ctx.kind, {
--- 										mode = "symbol",
--- 									})
--- 								end
---
--- 								return icon .. ctx.icon_gap
--- 							end,
---
--- 							-- Optionally, use the highlight groups from nvim-web-devicons
--- 							-- You can also add the same function for `kind.highlight` if you want to
--- 							-- keep the highlight groups in sync with the icons.
--- 							highlight = function(ctx)
--- 								local hl = ctx.kind_hl
--- 								if vim.tbl_contains({ "Path" }, ctx.source_name) then
--- 									local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
--- 									if dev_icon then
--- 										hl = dev_hl
--- 									end
--- 								end
--- 								return hl
--- 							end,
--- 						},
--- 					},
--- 				},
---
--- 				border = "rounded",
--- 			},
--- 			ghost_text = { enabled = false },
--- 		},
---
--- 		-- Default list of enabled providers defined so that you can extend it
--- 		-- elsewhere in your config, without redefining it, due to `opts_extend`
--- 		sources = {
--- 			default = { "lsp", "path", "snippets", "buffer" },
--- 			providers = {
--- 				buffer = {
--- 					opts = {
--- 						-- Complete from all open buffers
--- 						get_bufnrs = vim.api.nvim_list_bufs,
--- 					},
--- 				},
--- 			},
--- 		},
---
--- 		-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
--- 		-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
--- 		-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
--- 		--
--- 		-- See the fuzzy documentation for more information
--- 		fuzzy = { implementation = "prefer_rust_with_warning" },
--- 	},
--- 	opts_extend = { "sources.default" },
--- }
