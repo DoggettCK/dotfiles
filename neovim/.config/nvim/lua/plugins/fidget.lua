bbc2b7f0f880c2827f13339126ead85f015085a8
Chris Doggett <doggettc@gmail.com>
2025-08-27 22:47:42 -0500

    Fixing elixir autocompletion

--- neovim/.config/nvim/lua/plugins/fidget.lua
+++ neovim/.config/nvim/lua/plugins/fidget.lua
@@ -1,4 +1,16 @@
+-- Useful status updates for LSP.
 return {
 	"j-hui/fidget.nvim",
-	opts = {},
+	opts = {
+		progress = {
+			display = {
+				done_icon = "✓", -- Icon shown when all LSP progress tasks are complete
+			},
+		},
+		notification = {
+			window = {
+				winblend = 0, -- Background color opacity in the notification window
+			},
+		},
+	},
 }
