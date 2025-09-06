ce117e7b9fe461457ed4d87da26ca717eded4b2e
Chris Doggett <doggettc@gmail.com>
2025-09-06 16:27:00 -0500

    Fixed autocomplete using blink.cmp

---
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,50 @@
+return {
+	"mason-org/mason-lspconfig.nvim",
+	opts = {},
+	dependencies = {
+		{ "mason-org/mason.nvim", opts = {} },
+		"neovim/nvim-lspconfig",
+		{
+			"WhoIsSethDaniel/mason-tool-installer.nvim",
+			opts = {
+				integrations = { ["mason-lspconfig"] = true },
+			},
+		},
+		{
+			"j-hui/fidget.nvim",
+			opts = {
+				-- options
+			},
+		},
+	},
+	config = function()
+		require("mason").setup()
+
+		require("mason-lspconfig").setup({
+			automatic_installation = true,
+			ensure_installed = {
+				"cssls",
+				"eslint",
+				"html",
+				"jsonls",
+				"ts_ls",
+				"pyright",
+				"tailwindcss",
+				"expert",
+			},
+		})
+
+		require("mason-tool-installer").setup({
+			ensure_installed = {
+				"prettier",
+				"prettierd",
+				"stylua", -- lua formatter
+				"isort", -- python formatter
+				"black", -- python formatter
+				"pylint",
+				"eslint_d",
+				"expert",
+			},
+		})
+	end,
+}
