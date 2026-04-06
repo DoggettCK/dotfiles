3954efe9172c9d3d8a66434b90cefae555101069
Chris Doggett <doggettc@gmail.com>
2026-04-06 14:43:30 -0500

    Properly starting treesitter

--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,16 @@
+-- Highlight, edit, and navigate code
 return {
 	"nvim-treesitter/nvim-treesitter",
-	lazy = false,
+	version = false,
 	build = ":TSUpdate",
+	event = { "BufReadPost", "BufNewFile" },
+	config = function()
+		-- NOTE: Ensure you've run `:TSInstall <language>` if you're not getting highlighting
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = { "elixir", "eex", "heex", "rust", "ruby", "python", "cs", "typescript", "javascript" },
+			callback = function()
+				vim.treesitter.start()
+			end,
+		})
+	end,
 }
